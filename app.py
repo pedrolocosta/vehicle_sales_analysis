f67152e36a14f66955441d06e91fef19bc92ccd1
Pedro Costa <60372240+pedrolocosta@users.noreply.github.com>
2026-08-16 00:19:09 -0300

    adding main file

---
+++ app.py
@@ -0,0 +1,21 @@
+# importing the necessary libraries
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+# reading the data file
+car_data = pd.read_csv('vehicles.csv') 
+
+# creating a button to create a histogram
+hist_button = st.button('Criar histograma') 
+
+# if the button is clicked
+if hist_button: 
+    # writing a message to the user
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    # crating a histogram using Plotly Express
+    fig = px.histogram(car_data, x="odometer")
+
+    # showing a Plotly interactive chart
+    st.plotly_chart(fig, use_container_width=True)
